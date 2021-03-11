d28118c6615197e604373a6df4bcd6db49dea005
Abrham Semere <abrhamsemere@Abrhams-MacBook-Pro.local>
2021-03-10 21:27:10 -0600

    tobeupdated

---
+++ index.js
@@ -0,0 +1,38 @@
+// "use strict";
+
+
+function sum (){
+    let c = parseInt(document.getElementById("first").value);
+    let d = parseInt(document.getElementById("second").value)
+    let result = c + d;
+document.getElementById('result').value = result
+}
+
+function sub (){
+    let c = parseInt(document.getElementById("first").value);
+    let d = parseInt(document.getElementById("second").value)
+    let result = c - d;
+    document.getElementById('result').value = result
+}
+
+function mult (){
+    let c = parseInt(document.getElementById("first").value);
+    let d = parseInt(document.getElementById("second").value);
+    let result = c * d;
+    document.getElementById('result').value = result
+}
+
+function calculator(operator){
+    if (operator === '+'){
+        sum()
+    }
+    else if (operator === '-'){
+        sub()
+    }
+    else if (operator === '*'){
+        mult()
+    }
+}
+
+ 
+ 
